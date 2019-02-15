adc5eed846a4b769c6d4c89059d1d7234d42b9b1
rayGreycloud <rayGreycloud@gmail.com>
2019-02-14 16:22:18 -0800

    Add emojis and fn logic

--- handler.js
+++ handler.js
@@ -1,14 +1,25 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
+const emojis = [
+  '😄', '😃', '😀', '😊', '😉', '😍', '🔶', '🔷', '🚀'
+];
+
+module.exports.getRank = async (event, context, callback) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
+  
+  const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+    },
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: 'Rank generated!',
+      input: rankEmoji,
     }),
   };
 
+  callback(null, response);
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
